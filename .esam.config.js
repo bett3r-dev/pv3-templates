a103c8db14f6290aee69bfa50acac86f15ceef16
Tomas Ruiz <tomasruizr@gmail.com>
2023-05-07 12:15:16 -0300

    fix: add .esam.config.js file

---
+++ .esam.config.js
@@ -0,0 +1,7 @@
+/** @type {import('@bett3r-dev/pv3-utils-cli').CLIOptions} */
+module.exports = {
+  domainDirectory: './domainEvents',
+  servicesGlobPatterns: ['./services/**/*', './service'],
+  webappDirectory: './webapp',
+  clientLibraryDirectory: './clientLibrary'
+};
