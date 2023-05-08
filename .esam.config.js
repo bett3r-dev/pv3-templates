0775db5ebed56af7d284aadb1f880f317e5fcb02
Tomas Ruiz <tomasruizr@gmail.com>
2023-05-08 01:52:29 -0300

    fix: esam.config.js => esam.config.json

--- .esam.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('@bett3r-dev/pv3-utils-cli').CLIOptions} */
-module.exports = {
-  templatesUrlBase: 'https://github.com/bett3r-dev/pv3-templates/archive/refs/heads/',
-  domainDirectory: './domain-events',
-  servicesPath: '.',
-  createWebApp: true,
-  webappDirectory: './webapp',
-  clientLibraryDirectory: './client-library'
-};
