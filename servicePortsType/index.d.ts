83e97bff6db11aa0d54374497153425335a3989e
Tomas Ruiz <tomasruizr@gmail.com>
2024-05-25 12:43:14 -0300

    fix: add service ports customizable types

---
+++ servicePortsType/index.d.ts
@@ -0,0 +1,9 @@
+// This file is here to extend the base ports type with the specific ports for this service
+// Add the types that matches the ports you need for this service
+
+import { Ports as BasePorts } from "@bett3r-dev/pv3-types";
+
+/**
+ * Ports exposed to all modules in the service
+ */
+export type Ports = BasePorts
